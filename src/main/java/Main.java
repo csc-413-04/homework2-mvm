fecf91365c924cde29d5bc67afaf52612a7014c0
Mei <meili4119@gmail.com>
2018-10-23 15:48:01 -0700

    newuser endpoint working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,99 @@
 package main.java;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import spark.Request;
+import spark.Response;
+
+import java.util.Set;
+
+import static com.mongodb.client.model.Filters.eq;
 import static spark.Spark.*;
 
 public class Main {
+    public static String processRoute(Request req, Response res) {
+        Set<String> params = req.queryParams();
+        //System.out.println("Parameters: " + params);
+
+        // possible for query param to be an array
+        for (String param : params) {
+            System.out.println(param + " : " + req.queryParamsValues(param)[0]);
+        }
+        return "done!";
+    }
 
     public static void main(String[] args) {
-      // staticFiles.externalLocation("public");
-      // http://sparkjava.com/documentation
-      port(1234);
-      // calling get will make your app start listening for the GET path with the /hello endpoint
-      get("/hello", (req, res) -> "Hello World");
+        // open connection
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        // get ref to database
+        MongoDatabase db = mongoClient.getDatabase("REST2");
+        // get ref to collection
+        MongoCollection<Document> userCollection = db.getCollection("users");
+        MongoCollection<Document> authCollection = db.getCollection("auth");
 
+        /*
+        // create a new document
+        Document userDoc = new Document("username", "user2")
+                .append("password", "5678");
+        // insert document into collection
+        userCollection.insertOne(userDoc);
+
+
+       // create a new document
+       Document authDoc = new Document("username", "user1")
+               .append("timestamp", "12:00");
+       // insert document into collection
+       authCollection.insertOne(authDoc);
+
+*/
+
+        staticFiles.externalLocation("public");
+        // http://sparkjava.com/documentation
+        port(27017);
+
+        // calling get will make your app start listening for the GET path with the /hello endpoint
+        get("/newuser",(req, res) -> {
+            processRoute(req, res);
+            // create a new document
+            // insert document into collection
+            String name = req.queryParams("username");
+            String pass = req.queryParams("password");
+            System.out.println("Param" +  name + "&" + pass);
+            Document userDoc = new Document("username", name)
+                    .append("password", pass);
+            userCollection.insertOne(userDoc);
+            return "okay";
+        });
+
+        get("/login", (req, res) -> {
+            processRoute(req, res);
+            //if username and login correct then return hash or token
+
+            //if username correct and password incorrect then login fail
+            return "login_failed";
+        });
+
+        get("/addfriend", (req, res) -> {
+            processRoute(req, res);
+
+            //if bad token and friends user ID then fail
+            if (true) {
+                return "failed_authentication";
+                //if token and friends user id correct then ok
+            } else
+                return "okay";
+        });
+
+        get("/friends", (req, res) -> {
+            processRoute(req, res);
+            return "otherfriendsuserid";
+        });
+
+        // fetching a value from a search
+//        Document search = userCollection.find(eq("username", "leon")).first();
+//        System.out.println("serach result: " +search.getString("password"));
     }
 }
+
+
